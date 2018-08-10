164276b9cbeafb3ed9107d19f7c137300c67c28b
zhouquan <306757130@qq.com>
2018-08-10 18:59:19 +0800

    用户管理功能

--- application/admin/controller/User.php
+++ application/admin/controller/User.php
@@ -13,13 +13,56 @@ class User extends Auth
             $map['username']=array('like','%'.$find.'%');
         }
         $map['is_admin']  = array('eq','是');
+        $count = Db::name('users')->where($map)->count();
         $users = Db::name('users')->where($map)->order('uid asc')->paginate(10,false,['query'=>request()->param()]);
+        $this->assign('count',$count);
         $this->assign('users',$users);
         return $this->fetch();
     }
     
     public function userList(){
+        if(Request::instance()->isGet()){
+            $find = input('find');
+            $map['username']=array('like','%'.$find.'%');
+        }
+        $map['is_admin']  = array('eq','否');
+        $count = Db::name('users')->where($map)->count();
+        $users = Db::name('users')->where($map)->order('uid asc')->paginate(10,false,['query'=>request()->param()]);
+        $this->assign('count',$count);
+        $this->assign('users',$users);
         return $this->fetch();
+    }
+    
+    public function add(){
+        if(Request::instance()->isPost()){
+            $data['tagname'] = input('post.tagname');
+            $map['tagname'] = $data['tagname'];
+            $newTag =  Db::name('tag')->where($map)->find();
+            if($newTag){
+                $status = array(
+                    'status'    => 0,
+                    'message'   => '添加失败，标签已存在！',
+                );
+                return $status;
+            }
+            $re =  Db::name('tag')->insert($data);
+            $tagId = Db::name('user')->getLastInsID();
+            if($re || !empty($newTag)){
+                $status = array(
+                    'status'    => 1,
+                    'tagId'     => $tagId,
+                    'message'   => '添加成功',
+                );
+            }else{
+                $status = array(
+                    'status'    => 0,
+                    'message'   => '添加失败',
+                );
+            }
+            return $status;
+        }else{
+            return $this->fetch();
+        }
     }
     
     public function editStatus(){
