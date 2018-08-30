d0521ba3de7c4fb272db7089e3354e03d4b7fa7f
zhouquan <306757130@qq.com>
2018-08-30 18:49:36 +0800

    添加顶级分类

--- application/admin/controller/Category.php
+++ application/admin/controller/Category.php
@@ -2,6 +2,7 @@
 namespace app\admin\controller;
 
 use \think\Db;
+use \think\Request;
 
 class Category extends Auth
 {
@@ -10,11 +11,12 @@ class Category extends Auth
 
     	//获取总条数
          $count = Db::name('category')->count();
-//         $category = Db::name('category')->order('cid asc')->paginate(10);
-        $category = Db::name('category')->order('cid asc')->select();
-        $data=$this->getTree($category);
+//        $category = Db::name('category')->order('cid asc')->paginate(10);
+//        $category = Db::name('category')->order('cid asc')->select();
+//        $data=$this->getTree($category);
+//        $this->assign('data',$data);
         $this->assign('count',$count);
-        $this->assign('data',$data);
+        
         return $this->fetch();
     }
     
@@ -26,18 +28,49 @@ class Category extends Auth
         $data['data'] = $category;
         return json($data);
     }
-//
-    //无限极递归
-    public function getTree($data,$pid=0,$level=0){
-        static $arr=array();
-        foreach($data as $key=>$value){
-            if($value['pid'] == $pid){
-                $value['level']=$level;     //用来作为在模版进行层级的区分
-                $arr[] = $value;            //把内容存进去
-                $this->getTree($data,$value['cid'],$level+1);    //回调进行无线递归
+    
+    public function addTopCate(){
+        if(Request::instance()->isPost()){
+            $data['cname'] = input('post.cname');
+            $data['csort'] = input('post.csort');
+            $data['is_show'] = input('post.is_show');
+            $map['cname'] = $data['cname'];
+            $newCate =  Db::name('category')->where($map)->find();
+            if($newCate){
+                $status = array(
+                    'status'    => 0,
+                    'message'   => '添加失败，分类已存在！',
+                );
+                return $status;
             }
+            $re =  Db::name('category')->insert($data);
+            if($re){
+                $status = array(
+                    'status'    => 1,
+                    'message'   => '添加成功',
+                );
+            }else{
+                $status = array(
+                    'status'    => 0,
+                    'message'   => '添加失败',
+                );
+            }
+            return $status;
+        }else{
+            return $this->fetch();
         }
-        return $arr;
-        
     }
+    
+    //无限极递归
+//    public function getTree($data,$pid=0,$level=0){
+//        static $arr=array();
+//        foreach($data as $key=>$value){
+//            if($value['pid'] == $pid){
+//                $value['level']=$level;     //用来作为在模版进行层级的区分
+//                $arr[] = $value;            //把内容存进去
+//                $this->getTree($data,$value['cid'],$level+1);    //回调进行无线递归
+//            }
+//        }
+//        return $arr;
+//    }
 }
